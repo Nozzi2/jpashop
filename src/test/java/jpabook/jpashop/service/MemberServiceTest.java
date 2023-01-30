0475adbf367a45bb11d9d02e635d0f5356fe5159
노정환 <zdsa43@gmail.com>
2023-01-30 21:07:41 +0900

    영한님 강의 보관

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,112 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+
+    @Test
+    @Rollback(value = false)
+    public void 회원가입() throws Exception {
+        //Given
+        Member member = new Member();
+        member.setName("kim");
+        //When
+        Long saveId = memberService.join(member);
+        //Then
+        assertEquals(member, memberRepository.findOne(saveId));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void 중복_회원_예외() throws Exception {
+        //given
+        Member member1 = new Member();
+        member1.setName("kim2");
+
+        Member member2 = new Member();
+        member2.setName("kim2");
+
+        //when
+        memberService.join(member1);
+        memberService.join(member2); //여기에서 kim2를 두개 등록하는 것이기 때문에 예외가 발생해야한다!
+        //여기서 터져야되는데 아래까지 동작할경우
+
+        //then
+        fail("예외가 발생해야한다."); //정상적으로 동작했어도 이문장을 만나면 예외를 출력해준다.
+    }
+
+}
+/*
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+class OrderServiceTest {
+
+
+
+    @Autowired  EntityManager em;
+    @Autowired OrderService orderService;
+    @Autowired OrderRepository orderRepository;
+
+    @Test
+    public void 상품주문() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("회원1");
+        member.setAddress(new Address("서울","강가","123-123"));
+        em.persist(member);
+
+        Book book = new Book();
+        book.setName("시골 JPA");
+        book.setPrice(10000);
+        book.setStockQuantity(10);
+
+        int orderCount = 2;
+
+        //when
+        Long orderId = orderService.order(member.getId(), book.getId(), orderCount);
+
+        //then
+        Order getOrder = orderRepository.findOne(orderId);
+
+        assertEquals("상품 주문시 상태는 ORDER", OrderStatus.ORDER, getOrder.getStatus());
+        assertEquals("주문한 상품 종류 수가 정확해야 한다.", 1, getOrder.getOrderItems().size());
+        assertEquals("주문 가격은 가격 * 수량이다.", 10000 * orderCount, getOrder.getTotalPrice());
+        assertEquals("주문 수량만큼 재고가 줄어야 한다.", 10, book.getStockQuantity());
+    }
+
+    @Test
+    public void 주문취소() throws Exception {
+        //given
+
+        //when
+
+        //then
+    }
+
+    @Test
+    public void 상품주문_재고수량초과() throws Exception {
+        //given
+
+        //when
+
+        //then
+    }
+}
+ */
